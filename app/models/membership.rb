687039837a0c784122fa553e28c6482471b6e781
Jeroen Offerijns <jeroen@offerijns.nl>
2013-07-07 11:42:20 +0200

    Removed old code

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,13 +3,4 @@ class Membership < ActiveRecord::Base
 
   belongs_to :group
   belongs_to :user
-
-  after_save :add_assignment
-
-  def add_assignment
-   # if self.users.length >= 2
-      Assignment.create(content: "Test", deadline: 12.hours.from_now, user_id: user_id, group_id: group_id, finished: false)
-    #end
-  end
-
 end

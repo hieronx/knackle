9b44df7117b9f9adc88babd3d1aa692de847c6e9
Jeroen Offerijns <jeroen@offerijns.nl>
2013-07-07 12:35:43 +0200

    Removed S3 setup

--- config/initializers/paperclib.rb
+++ config/initializers/paperclib.rb
@@ -1,3 +1,3 @@
-Paperclip.interpolates(:s3_sg_url) do |att, style| 
-"#{att.s3_protocol}://s3-website-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
-end
+# Paperclip.interpolates(:s3_sg_url) do |att, style| 
+# "#{att.s3_protocol}://s3-website-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+# end
